ec03f4ba5edbfdaadb07e9943c8c64baf43dcdea
HCLOpenBMC <HCL_OpenBMC@hcl.com>
2020-10-19 20:52:58 +0530

    added functionality for HostSync mode

--- manager.cpp
+++ manager.cpp
@@ -10,7 +10,6 @@
 #include <xyz/openbmc_project/Common/error.hpp>
 
 namespace rules = sdbusplus::bus::match::rules;
-
 namespace // anonymous
 {
 
@@ -26,6 +25,7 @@ namespace time
 {
 
 using namespace phosphor::logging;
+using sdbusplus::exception::SdBusError;
 
 Manager::Manager(sdbusplus::bus::bus& bus) : bus(bus), settings(bus)
 {
@@ -45,6 +45,7 @@ Manager::Manager(sdbusplus::bus::bus& bus) : bus(bus), settings(bus)
 void Manager::onPropertyChanged(const std::string& key,
                                 const std::string& value)
 {
+
     assert(key == PROPERTY_TIME_MODE);
 
     // Notify listeners
@@ -109,10 +110,33 @@ bool Manager::setCurrentTimeMode(const std::string& mode)
     }
 }
 
+static constexpr auto systemdBusname = "org.freedesktop.systemd1";
+static constexpr auto systemdPath = "/org/freedesktop/systemd1";
+static constexpr auto systemdInterface = "org.freedesktop.systemd1.Manager";
+
+void Manager::updateHostSyncSetting(const std::string& mode){
+    auto method = bus.new_method_call(systemdBusname,systemdPath ,
+                                      systemdInterface, "StartUnit");
+    auto ServiceFile = "time-manager-bmc-set-time.service";
+    method.append(ServiceFile, "replace");
+    try
+    {
+        bus.call_noreply(method);
+        log<level::INFO>("Updated HostSync setting");
+    }
+    catch (const SdBusError &e)
+    {
+        log<level::ERR>("failed to update HostSync setting.",entry("ERR=%s", e.what()));
+    }
+}
+
 void Manager::onTimeModeChanged(const std::string& mode)
 {
-    // When time_mode is updated, update the NTP setting
-    updateNtpSetting(mode);
+   if(mode == "xyz.openbmc_project.Time.Synchronization.Method.HostSync")
+       updateHostSyncSetting(mode); //HostSync mode
+   else
+       updateNtpSetting(mode); //NTP Mode
+
 }
 
 std::string Manager::getSetting(const char* path, const char* interface,
